48c153a533dddb1918a19ef9fcafc84afdc8a20a
danielfinnerty <danielfinnerty@hotmail.com>
2025-10-14 18:51:58 +0100

    Updated assignment02

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -10,28 +10,31 @@ response = requests.get(url)
 data = response.json()
 nirl_data = (data['northern-ireland']) # seperate all the northern ireland dates from the rest
 
-# print('Northern Ireland 2024 Bank Holidays:') # this pulls all holidays in the file, for every year
+# print('Northern Ireland 2024 Bank Holidays:') # this pulls all holidays in the file, without separation for each year.
+
+# To outline holidays for each year:
+print('Northern Ireland 2024 Bank Holidays:')
 
 # Create loop to pull all the events that occur on a date which starts with 2024.
 for event in nirl_data['events']:
     if event['date'].startswith('2024'): # Startswith reference: https://www.w3schools.com/python/ref_string_startswith.asp
         print(f'{event["title"]} occurs on {event["date"]}')
 
-print('\n','Northern Ireland 2025 Bank Holidays:')
+print('\n''Northern Ireland 2025 Bank Holidays:')
 
 # Repeat look for 2025 and so on.
 for event in nirl_data['events']:
-    if event['date'].startswith('2025'): # Startswith reference: https://www.w3schools.com/python/ref_string_startswith.asp
+    if event['date'].startswith('2025'):
         print(f'{event["title"]} occurs on {event["date"]}')
 
 print('\n''Northern Ireland 2026 Bank Holidays:')
 
 for event in nirl_data['events']:
-    if event['date'].startswith('2026'): # Startswith reference: https://www.w3schools.com/python/ref_string_startswith.asp
+    if event['date'].startswith('2026'):
         print(f'{event["title"]} occurs on {event["date"]}')
 
 print('\n''Northern Ireland 2027 Bank Holidays:')
 
 for event in nirl_data['events']:
-    if event['date'].startswith('2027'): # Startswith reference: https://www.w3schools.com/python/ref_string_startswith.asp
+    if event['date'].startswith('2027'):
         print(f'{event["title"]} occurs on {event["date"]}')
